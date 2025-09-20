ca2f629c0bbc467040e3b263e915a6ed61301d57
oussama-zbair <oussama.zbair9@gmail.com>
2025-09-20 11:10:49 +0100

    Update currecnyController and currencyService

--- src/main/java/com/currencyxchange/controller/CountryCurrencyController.java
+++ src/main/java/com/currencyxchange/controller/CountryCurrencyController.java
@@ -2,23 +2,21 @@ package com.currencyxchange.controller;
 
 import com.currencyxchange.dto.CountryCurrencyDTO;
 import com.currencyxchange.service.CountryCurrencyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/countries")
+@RequiredArgsConstructor
 public class CountryCurrencyController {
 
     private final CountryCurrencyService countryCurrencyService;
 
-    public CountryCurrencyController(CountryCurrencyService countryCurrencyService) {
-        this.countryCurrencyService = countryCurrencyService;
-    }
-
-    @GetMapping("/countries")
-    public Flux<CountryCurrencyDTO> getCountries() {
+    @GetMapping
+    public Flux<CountryCurrencyDTO> getAllCountriesWithCurrency() {
         return countryCurrencyService.getAllCountriesWithCurrency();
     }
 }
